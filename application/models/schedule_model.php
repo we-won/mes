258ec1788100436bb7761166cb39de588b682ce3
carlo dinopol <carlo.dinopol@gmail.com>
2016-03-04 23:07:44 +0800

    +enroll new fix

--- application/models/schedule_model.php
+++ application/models/schedule_model.php
@@ -185,17 +185,18 @@ class schedule_model extends CI_Model
 
 	public function get_sy_schedules()
 	{
-		$this->load->model('schoolyear_model');
-
 		$sy_id = $this->schoolyear_model->get_active_sy()['id'];
 
 		$q = $this->db->query("
-			SELECT a.id, a.schoolyear_id, a.subject_id,
-			b.title as subject_title, b.units
-			FROM mes_schedule a
-			LEFT JOIN mes_subjects b ON a.subject_id = b.id
-			WHERE a.schoolyear_id = $sy_id
-			AND a.is_active = 1
+			SELECT a.id, b.title as subject_title, b.description as subject_description, 
+			GROUP_CONCAT(d.code1, '(', c.start_time, ' - ', c.end_time, ')' ORDER BY c.day SEPARATOR ', ') as schedule 
+			FROM mes_schedule a 
+			LEFT JOIN mes_subjects b ON a.subject_id = b.id 
+			LEFT JOIN mes_subject_schedule c ON c.schedule_id = a.id 
+			LEFT JOIN mes_days d ON d.id = c.day
+			AND a.is_active <> 0 
+			AND a.schoolyear_id = $sy_id 
+			GROUP BY a.id 
 		");
 		
 		return $q->result();
